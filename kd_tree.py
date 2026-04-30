3fc39e43ac907f3054f0a4f8d4a6992def0393a4
Rodrigo Pierre <rodrigopierre05@gmail.com>
2026-04-30 19:38:22 -0300

    feat: kd tree implemented

---
+++ kd_tree.py
@@ -0,0 +1,74 @@
+from dataclasses import dataclass
+from typing import Optional, List
+
+@dataclass
+class Point:
+    name: str
+    address: str
+    lat: float
+    lon: float
+
+@dataclass
+class KDNode:
+    point: Point
+    axis: int
+    left: Optional["KDNode"] = None
+    right: Optional["KDNode"] = None
+
+class KDTree:
+    def __init__(self, points: List[Point]):
+        valid_points = [
+            item
+            for item in points
+            if item.lat is not None
+            and item.lon is not None
+        ]
+
+        self.root = self._create_tree(
+            valid_points,
+            0,
+        )
+
+    def _create_tree(
+        self,
+        values: List[Point],
+        level: int,
+    ) -> Optional[KDNode]:
+        if len(values) == 0:
+            return None
+        current_axis = level % 2
+        sorted_points = sorted(
+            values,
+            key=lambda item: (
+                item.lat
+                if current_axis == 0
+                else item.lon
+            ),
+        )
+        middle_index = len(sorted_points) // 2
+        middle_point = sorted_points[middle_index]
+        left_branch = self._create_tree(
+            sorted_points[:middle_index],
+            level + 1,
+        )
+        right_branch = self._create_tree(
+            sorted_points[middle_index + 1:],
+            level + 1,
+        )
+        return KDNode(
+            point=middle_point,
+            axis=current_axis,
+            left=left_branch,
+            right=right_branch,
+        )
+
+    def __len__(self):
+        def node_count(current_node):
+            if current_node is None:
+                return 0
+            return (
+                1
+                + node_count(current_node.left)
+                + node_count(current_node.right)
+            )
+        return node_count(self.root)
