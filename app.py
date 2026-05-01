aff6b2da5c7ee38761695f4796ae6b2f582fe335
Márcia Bigão <mbigaod@gmail.com>
2026-05-01 16:37:53 -0300

    ler locais

---
+++ app.py
@@ -0,0 +1,44 @@
+import csv
+from dash import Dash, html, dcc, dash_table, ctx
+from dash.dependencies import Input, Output, State
+import dash_leaflet as dl
+from kd_tree import Point, KDTree
+from search import search_by_diagonal
+from geocode import geocode_user_address
+
+CSV_PATH = "data/butecos_geocoded.csv"
+MAP_CENTER = (-19.9167, -43.9345)
+
+def read_locations():
+    locations = []
+
+    with open(
+        CSV_PATH,
+        newline="",
+        encoding="utf-8",
+    ) as file:
+        csv_reader = csv.DictReader(file)
+
+        for entry in csv_reader:
+            latitude = entry["lat"].strip()
+            longitude = entry["lon"].strip()
+
+            if (
+                not latitude
+                or not longitude
+                or latitude == "None"
+                or longitude == "None"
+            ):
+                continue
+
+            location = Point(
+                name=entry["name"].strip(),
+                address=entry["address"].strip(),
+                lat=float(latitude),
+                lon=float(longitude),
+            )
+            locations.append(location)
+    return locations
+
+LOCATIONS = read_locations()
+SEARCH_TREE = KDTree(LOCATIONS)
